7f5dff90edf20154a4ed1b5fec50520e269ddb42
jcarmonabastida-pixel <jcarmonabastida@gmail.com>
2026-09-22 18:40:02 +0200

    TGCV TR-131: instrument ESEBProvider BusServer exception

---
+++ 03_EXPERIMENTS/TR-131/diagnostics/patch_esebprovider_busserver_diag.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+TARGET = Path("rainbow/rainbow-core/src/main/java/org/sa/rainbow/core/ports/eseb/ESEBProvider.java")
+
+text = TARGET.read_text(encoding="utf-8")
+if "[TR131-DIAG-ESEB-PROVIDER]" in text:
+    print("ALREADY_PATCHED")
+    raise SystemExit(0)
+
+old = """            catch (Exception e) {
+                ESEBConnector.LOGGER.warn (MessageFormat.format ("BusServer could not be created on port {0}", port));
+            }
+"""
+new = """            catch (Exception e) {
+                System.err.println("[TR131-DIAG-ESEB-PROVIDER] EXCEPTION_CLASS=" + e.getClass().getName());
+                System.err.println("[TR131-DIAG-ESEB-PROVIDER] EXCEPTION_MESSAGE=" + e.getMessage());
+                e.printStackTrace(System.err);
+                ESEBConnector.LOGGER.warn (MessageFormat.format ("BusServer could not be created on port {0}", port));
+            }
+"""
+if old not in text:
+    raise SystemExit("PATCH_BLOCK_NOT_FOUND")
+
+text = text.replace(old, new, 1)
+TARGET.write_text(text, encoding="utf-8")
+print("PATCH_OK")
